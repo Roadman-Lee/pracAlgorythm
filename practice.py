e742cee1e7e8f9a1a0d3a817e21f1c8e97aea118
Roadman <rdm@Roadmanui-iMac.local>
2021-12-21 08:29:18 +0900

    파이썬 기초예제, 알고리즘 21년 12월 20일

--- practice.py
+++ practice.py
@@ -1,8 +1,23 @@
-warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
-user = input("회사 이름: ")
-for i in range(len(warn_investment_list)):
-    if warn_investment_list[i] == user:
-        print("투자 경고 종목이 아닙니다")
-        break
-    print("투자 경고 종목입니다")
-    break    
+def com_up_down():
+    max_num = 100
+    min_num = 1
+    user = int(input("1~100사이의 수를 입력하시오 : "))
+    while True:
+        com_num = min_num +((max_num-min_num) // 2) 
+        print(com_num)
+        if com_num == user:
+            return print("정답")
+        elif com_num > user:
+            userAnswer = str(input())
+            while userAnswer != "다운":
+                print("다시 입력하시오.")
+                userAnswer = str(input())
+            max_num = com_num 
+        else:
+            userAnswer = str(input())
+            while userAnswer != "업":
+                print("다시 입력하시오.")
+                userAnswer = str(input())
+            min_num = com_num 
+            
+com_up_down()
